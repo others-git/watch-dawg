4e0a7354aa506ddf3fe58d1ba22ae5b32f4ab339
aaron mittag <beard1ess@theundead.live>
2023-04-01 21:06:09 -0500

    Revert "Settings and GUI updates (#5)"
    This reverts commit b5f8be688bb70b872d4f4f35cf35c369af156067.

--- gitops.go
+++ gitops.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
+	"fmt"
 	"time"
 
 	git "github.com/go-git/go-git/v5"
@@ -13,39 +12,39 @@ import (
 )
 
 func openRepository(projectPath string) (*git.Repository, error) {
-	r, err := git.PlainOpen(projectPath)
-	return r, err
+    r, err := git.PlainOpen(projectPath)
+    return r, err
 }
 
 func addToWorktreeAndCommit(r *git.Repository, gitUsername string) error {
-	w, err := r.Worktree()
-	if err != nil {
-		return err
-	}
+    w, err := r.Worktree()
+    if err != nil {
+        return err
+    }
 
-	_, err = w.Add(".")
-	if err != nil {
-		return err
-	}
+    _, err = w.Add(".")
+    if err != nil {
+        return err
+    }
 
-	commit, err := w.Commit("Ableton project file changed", &git.CommitOptions{
-		Author: &object.Signature{
-			Name:  gitUsername,
-			Email: fmt.Sprintf("%s@example.com", gitUsername),
-			When:  time.Now(),
-		},
-	})
-	if err != nil {
-		return err
-	}
+    commit, err := w.Commit("Ableton project file changed", &git.CommitOptions{
+        Author: &object.Signature{
+            Name:  gitUsername,
+            Email: fmt.Sprintf("%s@example.com", gitUsername),
+            When:  time.Now(),
+        },
+    })
+    if err != nil {
+        return err
+    }
 
-	obj, err := r.CommitObject(commit)
-	if err != nil {
-		return err
-	}
+    obj, err := r.CommitObject(commit)
+    if err != nil {
+        return err
+    }
 
-	log.Println("Created new commit:", obj)
-	return nil
+    log.Println("Created new commit:", obj)
+    return nil
 }
 
 func pushChanges(r *git.Repository, auth *http.BasicAuth) error {
@@ -60,23 +59,6 @@ func pushChanges(r *git.Repository, auth *http.BasicAuth) error {
 
 	if err != nil {
 		return err
-	}
-	return nil
-}
-
-// checkoutRepo checks if the repository exists locally, and clones it if it doesn't.
-func checkoutRepo(projectPath, gitRepoURL, gitUsername, gitPassword string) error {
-	if _, err := os.Stat(projectPath); os.IsNotExist(err) {
-		_, err := git.PlainClone(projectPath, false, &git.CloneOptions{
-			URL: gitRepoURL,
-			Auth: &http.BasicAuth{
-				Username: gitUsername,
-				Password: gitPassword,
-			},
-		})
-		if err != nil {
-			return fmt.Errorf("failed to clone repository: %w", err)
-		}
 	}
 	return nil
 }
